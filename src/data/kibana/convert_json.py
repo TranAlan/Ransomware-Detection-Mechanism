0bfee64f6d4eb29dc744908565f1a06cfea173fd
Peter Lam <peter.quach.lam@gmail.com>
2020-02-11 08:57:36 -0500

    Added JSON translator

---
+++ src/data/kibana/convert_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+'''This program provides the functions to convert a typical JSON to the BulkAPI requirements, and vice versa '''
+
+import sys
+import os.path as path
+import json
+
+
+def convert_to_bulk_api(json_path):
+    '''Takes regular json file and converts to bulk api json'''
+    print("hello")
+
